a6998c190928cf60e9de4c6e1928f699004d4c62
Sai Porumamilla <85517489+flamebot43261@users.noreply.github.com>
2024-07-04 12:49:32 -0400

    Update time_series_visualizer.py

--- time_series_visualizer.py
+++ time_series_visualizer.py
@@ -1,3 +1,6 @@
+#I used a walkthrough for this project given by Landon Schlangen through this YouTube video below: 
+# --> https://www.youtube.com/watch?v=889odedq2ww&t=281s
+
 import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
